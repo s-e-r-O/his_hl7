43be92de4ab0f0188891f21977288ae699a98c97
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2019-03-17 21:04:11 -0400

    develop: Added Physician Information System to handle doctor functions.

--- his/his_hl7/src/main/java/bioinfo/his_hl7/App.java
+++ his/his_hl7/src/main/java/bioinfo/his_hl7/App.java
@@ -2,5 +2,5 @@ package bioinfo.his_hl7;
 
 public class App 
 {	
-    public static void main(String[] args) throws Exception {}	
+    public static void main(String[] args) throws Exception { }	
 }
---
+++ his/pis_hl7/src/main/java/bioinfo/pis_hl7/App.java
@@ -0,0 +1,13 @@
+package bioinfo.pis_hl7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
