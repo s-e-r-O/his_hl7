7d4988938414b61301a8153a4dd6d18296353fba
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2019-03-19 19:53:57 -0400

    med: Made a singleton for the schedule manager

--- his/pis_hl7/src/main/java/bioinfo/pis_hl7/App.java
+++ his/pis_hl7/src/main/java/bioinfo/pis_hl7/App.java
@@ -2,18 +2,16 @@ package bioinfo.pis_hl7;
 
 public class App 
 {
-	private static Manager manager;
-	public static Manager getManager() {
+	private static ScheduleManager manager;
+	public static ScheduleManager getManager() {
 		if (manager == null) {
-			manager = new Manager();
+			manager = new ScheduleManager();
 		}
 		return manager;
 	}
 	
 	public static void main( String[] args )
     {
-    	Server s = new Server();
-    	s.init();
-    	
+		getManager().init();
     }
 }
