ff2b606f742e392354f9adbfeb155189b3b1c322
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2019-03-20 20:38:50 -0400

    develop: Added simple buttons

--- his/his_hl7/src/main/java/bioinfo/his_hl7/NurseMenuFrame.java
+++ his/his_hl7/src/main/java/bioinfo/his_hl7/NurseMenuFrame.java
@@ -6,6 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
 
 public class NurseMenuFrame extends JFrame {
 
@@ -32,11 +39,45 @@ public class NurseMenuFrame extends JFrame {
 	 */
 	public NurseMenuFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 172);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblEnfermero = new JLabel("Enfermero");
+		lblEnfermero.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEnfermero.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JSeparator separator = new JSeparator();
+		
+		JButton btnRegistrarVisitaDe = new JButton("Registrar visita de emergencia");
+		
+		JButton btnRegistroDeLlegada = new JButton("Registro de llegada");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblEnfermero, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE)
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnRegistrarVisitaDe, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnRegistroDeLlegada, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(lblEnfermero, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+					.addGap(7)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 9, GroupLayout.PREFERRED_SIZE)
+					.addGap(7)
+					.addComponent(btnRegistrarVisitaDe)
+					.addGap(13)
+					.addComponent(btnRegistroDeLlegada)
+					.addContainerGap(137, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
--- his/his_hl7/src/main/java/bioinfo/his_hl7/SecretaryMenuFrame.java
+++ his/his_hl7/src/main/java/bioinfo/his_hl7/SecretaryMenuFrame.java
@@ -6,6 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
 
 public class SecretaryMenuFrame extends JFrame {
 
@@ -32,11 +39,45 @@ public class SecretaryMenuFrame extends JFrame {
 	 */
 	public SecretaryMenuFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 171);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblSecretario = new JLabel("Secretario");
+		lblSecretario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSecretario.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JSeparator separator = new JSeparator();
+		
+		JButton btnAgendarConsulta = new JButton("Agendar consulta");
+		
+		JButton btnCancelarConsulta = new JButton("Cancelar consulta");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblSecretario, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE)
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnAgendarConsulta, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnCancelarConsulta, GroupLayout.PREFERRED_SIZE, 399, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(lblSecretario, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+					.addGap(7)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 9, GroupLayout.PREFERRED_SIZE)
+					.addGap(7)
+					.addComponent(btnAgendarConsulta)
+					.addGap(13)
+					.addComponent(btnCancelarConsulta)
+					.addContainerGap(137, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
