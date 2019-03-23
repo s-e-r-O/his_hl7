a4722e9d50d763bc3e7728e4f06e5fd7ef2e42f8
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2019-03-23 15:29:36 -0400

    develop: Added a file for global constants regarding network parameters

---
+++ his/models/src/main/java/upb/bio/global/NetworkConstants.java
@@ -0,0 +1,17 @@
+package upb.bio.global;
+
+public final class NetworkConstants {
+	public static int EXT_SERVER_PORT = 1011;
+	public static int PIS_SERVER_PORT = 1012;
+	public static int HIS_SERVER_PORT = 1013;
+	
+	
+	public static String EXT_REMOTE_HOST = "localhost";
+	public static int EXT_REMOTE_PORT = 1011;
+
+	public static String PIS_REMOTE_HOST = "localhost";
+	public static int PIS_REMOTE_PORT = 1012;
+
+	public static String HIS_REMOTE_HOST = "localhost";
+	public static int HIS_REMOTE_PORT = 1013;
+}
