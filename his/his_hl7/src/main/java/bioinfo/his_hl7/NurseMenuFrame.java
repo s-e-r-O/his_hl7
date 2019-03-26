13550a622280aa8c7a5277e4226b7510fbcedc98
Isabella Defilippis <isastedef@yahoo.com>
2019-03-25 20:03:24 -0400

    Fixed bugs

--- his/his_hl7/src/main/java/bioinfo/his_hl7/NurseMenuFrame.java
+++ his/his_hl7/src/main/java/bioinfo/his_hl7/NurseMenuFrame.java
@@ -24,7 +24,7 @@ public class NurseMenuFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public NurseMenuFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 172);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- his/his_hl7/src/main/java/bioinfo/his_hl7/SecretaryMenuFrame.java
+++ his/his_hl7/src/main/java/bioinfo/his_hl7/SecretaryMenuFrame.java
@@ -24,7 +24,7 @@ public class SecretaryMenuFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public SecretaryMenuFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 171);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
